917f148f137704085081e0a3321bc064e87d18c2
Joshua Carter <carter.joshua.603@gmail.com>
2025-07-06 19:06:27 -0700

    Fix: ignore output files in eslint - fixes linting errors (#2)

--- packages/mimisbrunnr-ext/eslint.config.mjs
+++ packages/mimisbrunnr-ext/eslint.config.mjs
@@ -1,6 +1,12 @@
 import baseConfig from '../../eslint.config.mjs';
 
 export default [
+    {
+        ignores: [
+            'packages/mimisbrunnr-ext/.output/**',
+            'packages/mimisbrunnr-ext/.wxt/**',
+        ],
+    },
     ...baseConfig,
     {
         files: ['**/*.json'],
