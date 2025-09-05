1921d8b05fc4cfc4223ad668d1fc6902113a5ce7
Joshua Carter <carter.joshua.603@gmail.com>
2025-09-04 22:03:33 -0700

    Rename perpetual-node package to orbitdb-manager

--- packages/orbitdb-manager/vite.config.ts
+++ packages/orbitdb-manager/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => ({
     root: __dirname,
-    cacheDir: '../../node_modules/.vite/packages/perpetual-node',
+    cacheDir: '../../node_modules/.vite/packages/orbitdb-manager',
     plugins: [],
     // Uncomment this if you are using workers.
     // worker: {
