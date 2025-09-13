8ed84456c4c9a2b66eb61e0c2749073d97ff237a
Joshua Carter <carter.joshua.603@gmail.com>
2025-09-13 11:41:30 -0700

    Rename orbitdb-manager to orbitdb-service

---
+++ packages/orbitdb-service/README.md
@@ -0,0 +1,11 @@
+# orbitdb-service
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build orbitdb-service` to build the library.
+
+## Running unit tests
+
+Run `nx test orbitdb-service` to execute the unit tests via [Vitest](https://vitest.dev/).
--- packages/orbitdb-service/vite.config.ts
+++ packages/orbitdb-service/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => ({
     root: __dirname,
-    cacheDir: '../../node_modules/.vite/packages/orbitdb-manager',
+    cacheDir: '../../node_modules/.vite/packages/orbitdb-service',
     plugins: [],
     // Uncomment this if you are using workers.
     // worker: {
