df7982e46f5dc4d39e6570dc7e0c074784275f87
Joshua Carter <carter.joshua.603@gmail.com>
2025-10-11 13:26:28 -0700

    MM-21 Setup docker stack (#23)
    * Install @nx/docker
    
    * Create Dockerfiles and docker-compose stack for services
    
    * Update lua scripts, rename validator to validation-service
    
    * Update nginx.conf to correctly work inside openresty container
    
    * Write WIP infra tests
    
    * Linting

--- packages/validation-proxy/vite.config.ts
+++ packages/validation-proxy/vite.config.ts
@@ -8,12 +8,12 @@ export default defineConfig(() => ({
     // worker: {
     //  plugins: [ nxViteTsPaths() ],
     // },
-    // TODO: Setup integration tests in MM-21
+    // TODO: Get integration tests working
     // test: {
     //     watch: false,
     //     globals: true,
     //     environment: 'node',
-    //     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    //     include: ['test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     //     reporters: ['default'],
     //     coverage: {
     //         reportsDirectory: './test-output/vitest/coverage',
