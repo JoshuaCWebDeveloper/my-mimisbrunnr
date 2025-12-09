73dec4ba29f6ffb9c7eb019809bada429af24526
Joshua Carter <carter.joshua.603@gmail.com>
2025-12-09 12:26:11 -0800

    Correct error, add record to discovery log using add(), not addOperation() in OrbitDbServiceManager

--- packages/orbitdb-service/src/services/orbitdb-service-manager.ts
+++ packages/orbitdb-service/src/services/orbitdb-service-manager.ts
@@ -212,7 +212,7 @@ export class OrbitDbServiceManager
                 throw new Error('Invalid discovery record');
             }
 
-            const hash = await this.discoveryLog.addOperation(record);
+            const hash = await this.discoveryLog.add(record);
 
             this.loggerService.info(`➕ Discovery record added`, {
                 hash,
